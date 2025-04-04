64a46e298225f6b849ef069c44263c10b0b39e2b
Yash <yashkokcha@outlook.com>
2025-04-04 14:18:25 +0530

    EDA & Problem Statement

--- setup.py
+++ setup.py
@@ -4,34 +4,28 @@ from setuptools import find_packages, setup
 ## For returning list we have to import list package from typing module
 from typing import List
 
+HYPEN_E_DOT = "-e ."
 
-HYPEN_E_DOT= '-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    This Function will return the list of requirements
-    '''
-    requirements= []
-    with open(file_path)as file_obj:
-        requirements= file_obj.readlines()
-        ## For not reading '\n' in requirements we'll replace with space
-        requirements= [req.replace('\n','') for req in requirements]
-
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This Function returns a list of package requirements.
+    """
+    requirements = []
+    with open(file_path, "r", encoding="utf-8") as file_obj:  # Added encoding
+        requirements = [req.strip() for req in file_obj.readlines()]  # Cleaner newline handling
+        
+        # Remove '-e .' if present
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
-
 setup(
-    name='MLproject',
-    version='0.0.1',
-    author='Yash',
-    author_email='yashkokcha@outlook.com',
-    packages= find_packages(),
-    install_requires= get_requirements('requirements.txt')
-
-
-
-
-
+    name="MLproject",
+    version="0.0.1",
+    author="Yash",
+    author_email="yashkokcha@outlook.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),  # Ensuring it's correctly read
 )
+
---
+++ src/components/__init__.py
@@ -0,0 +1,2 @@
+## Our main aim is to create components(modules) folder for using as a package so that it can be imported
+
